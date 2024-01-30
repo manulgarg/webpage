541056c751caae34541e1ca9d04811dac42c63e1
manulgarg <158170151+manulgarg@users.noreply.github.com>
2024-01-30 21:15:18 +0530

    Update 4

--- webpagetest.sh
+++ webpagetest.sh
@@ -2,6 +2,7 @@
 
 url_to_test="https://www.telenetix2.in/"
 
+
 response=$(curl -s -o /dev/null -w "%{http_code}" $url_to_test)
 if [ $response -eq 200 ]; then
     echo "URL Test Passed: $url_to_test is reachable."
